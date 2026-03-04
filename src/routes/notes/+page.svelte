2f4ae27df3a5fdd1d91016583be8476a6e79a1c2
hshmp <aarya.v.web@gmail.com>
2026-03-05 00:13:14 +1100

    small: change animation delays

--- src/routes/notes/+page.svelte
+++ src/routes/notes/+page.svelte
@@ -2,10 +2,10 @@
 
 <main>
 	<h1>Notes</h1>
-	<p>
+	<p data-delay="250">
 		Resources from both anonymous and named donors are posted here. They may be outdated or goated.
 	</p>
-	<p data-delay="250">
+	<p data-delay="500">
 		You can open markdown files (<code>.md</code>) using <a href="https://obsidian.org">Obsidian</a>
 		or a code editor like <a href="https://vscode.dev">Visual Studio Code</a>.
 	</p>
@@ -20,18 +20,18 @@
 		</li>
 	</ul>
 	<h2>Computing</h2>
-	<p data-delay="500">
+	<p data-delay="250">
 		The Foundations of Computing course is aimed at students with no experience in programming. If
 		you sat through the lecture thinking about wasted potential, you are not alone. However, free WAM
 		never hurt anybody.
 	</p>
-	<p>
+	<p data-delay="500">
 		If you decide to take the proficiency test and pass, you will not receive credit for Foundations of Computing
 		and instead be waived the requisite for the Algorithms course.
 	</p>
 	<h2>Algorithms</h2>
 	<h3>Segment Tree</h3>
-  <p>It has to be the segment tree...</p>
+  <p data-delay="250">It has to be the segment tree...</p>
 	<div class="img up">
 		<img src="segtree.png" alt="iterative segment tree diagram" />
 	</div>
