873e4ec853e29477541b231f94589ca6f4e0ea0b
hshmp <aarya.v.web@gmail.com>
2026-03-05 16:00:14 +1100

    fix: update notes page

--- src/routes/notes/+page.svelte
+++ src/routes/notes/+page.svelte
@@ -1,4 +1,6 @@
-<script></script>
+<script>
+const subjects = ['calculus', 'computing', 'linear_algebra', 'algorithms']
+</script>
 
 <main>
 	<h1>Notes</h1>
@@ -9,30 +11,36 @@
 		You can open markdown files (<code>.md</code>) using <a href="https://obsidian.md" target="_blank">Obsidian</a>
 		or a code editor like <a href="https://vscode.dev" target="_blank">Visual Studio Code</a>.
 	</p>
-	<h2>Calculus</h2>
-	<h3>Limits</h3>
-	<ul>
-		<li data-delay="250">
-			<a href="calc_ii_limits.md">
-				<span class="shortcut">Limits Markdown</span>
-				<span class="material-symbols-outlined">arrow_right_alt</span>
-			</a>
-		</li>
-	</ul>
-	<h2>Computing</h2>
-	<p data-delay="250">
-		The Foundations of Computing course is aimed at students with no experience in programming. If
-		you sat through the lecture thinking about wasted potential, you are not alone. However, free WAM
-		never hurt anybody.
-	</p>
-	<p data-delay="500">
-		If you decide to take the proficiency test and pass, you will not receive credit for Foundations of Computing
-		and instead be waived the requisite for the Algorithms course.
-	</p>
-	<h2>Algorithms</h2>
-	<h3>Segment Tree</h3>
-  <p data-delay="250">It has to be the segment tree...</p>
-	<div class="img up">
-		<img src="segtree.png" alt="iterative segment tree diagram" />
+	<h2>Subjects</h2>
+	<div class="linkgrid">
+		{#each subjects as subject, i}
+			<a href={`/notes/${subject}`} class="up" data-delay={(i + 1) * 250}>{subject.toUpperCase().replaceAll('_', ' ')}</a>
+		{/each}
 	</div>
 </main>
+
+<style>
+.linkgrid {
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+	gap: 1rem;
+}
+
+.linkgrid a {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 1rem;
+	background: linear-gradient(var(--accent-0)) 0% 0% / 0% 100% no-repeat;
+	border: 2px solid var(--accent-0);
+	border-radius: 0.5rem;
+	color: var(--text-0);
+	text-decoration: none;
+	font-weight: bold;
+	transition: background 250ms ease-in-out;
+}
+
+.linkgrid a:hover {
+	background-size: 100% 100%;
+}
+</style>
